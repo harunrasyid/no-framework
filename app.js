f52741f80ad3b6e4d3c95bc9e3cb0a3b661eb092
harunrasyid98@gmail.com <harunrasyid>
2023-11-06 20:51:11 +0700

    - Feat: setup express js - Feat: add nodemon for hot module replacement in the dev - Feat: init pug file

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const port = 3000;
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 app.get("/", (req, res) => {
-  res.render("Hello World!");
+  res.render("index");
 });
 
 app.listen(port, () => {
